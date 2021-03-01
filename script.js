061c0abb05695ea6db118df63f6b65625bc5f5c4
Fatlind Shehu <fatlind408@gmail.com>
2021-03-01 10:01:01 -0500

    Documenting how the code works

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 'use strict';
+// Selectiong the buttons or events we want to happen
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
-
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
+//Open Modal function
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
-
+//Close modal function
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+//Looping through Modals using for lopp, to reach each one and on click event to add some functionality
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal);
 }
-
+//Closing the modal in case of clicking the btn close or clicking in outside space off Modal
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
